4701d6e1a5db183acaf2a2182767e7985c0e7273
rainypin <zzy7186@163.com>
2015-06-16 15:57:33 +0800

    Update

--- Cookie.js
+++ Cookie.js
@@ -1,55 +1,59 @@
-!function(w,undefined) {
+/**
+ * Created by hzzhu_zhenyu on 2015/5/12.
+ */
 
-function Cookie() {
-    return this.init();
-}
+!function (w, undefined) {
 
-Cookie.prototype.init = function() {
-	console.log('w:' + w);
-	var _this = this;
-	_this.doc = w.document;
-	_this.escape = w.escape;
-	_this.unescape = w.unescape;		
+    function Cookie() {
+        return this.init();
+    }
 
-	return _this;
-};	
-Cookie.prototype.setCookie = function(cName,cValue,cExpiresDays) {
-	var exdate = new Date();　　　　
-	exdate.setDate(exdate.getDate() + cExpiresDays);　
-	if(cName == null) {
-		throw new Error("cookie name can't empty!");
-	} else {
-		this.doc.cookie = cName + "=" + this.escape(cValue) + ((cExpiresDays == null) ? "" : ";expires=" + exdate.toGMTString());　　　
-	}　　
-};
+    Cookie.prototype.init = function () {
+        var _this = this;
+        _this.doc = w.document;
+        _this.escape = w.escape;
+        _this.unescape = w.unescape;
 
-Cookie.prototype.getCookie = function(cName) {
-	var _value = "",
-		_cStart,
-		_cEnd,
-		doc = this.doc,
-		cookie = doc.cookie;
-	if(cName != null && cName !== "" && cookie.length > 0) {
-		_cStart = cookie.indexOf(cName + "=");
-		if(_cStart != -1) {
-			_cStart += (cName + "=").length;
-			_cEnd = cookie.indexOf(";",_cStart);
-			if(_cEnd == -1) {
-				_cEnd = cookie.length;
-			}
-			_value = this.unescape(cookie.substring(_cStart, _cEnd));
-		}
-	}
-	return _value;
-};
+        return _this;
+    };
+    Cookie.prototype.setCookie = function (cName, cValue, cExpiresDays) {
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + cExpiresDays);
+        if (cName == null) {
+            throw new Error("cookie name can't empty!");
+        } else {
+            console.log( cName + "=" + this.escape(cValue) + ((cExpiresDays == null) ? "" : ";expires=" + exdate.toGMTString()));
+            this.doc.cookie = cName + "=" + this.escape(cValue) + ((cExpiresDays == null) ? "" : ";expires=" + exdate.toGMTString());
+        }
+    };
 
-Cookie.prototype.deleteCookie = function(cName) {
-	var exdate = new Date();
-    exdate.setTime(exdate.getTime() - 1);
-    var cval=this.getCookie(cName);
-    if(cval!=null) this.doc.cookie= cName + "="+cval+";expires="+exdate.toGMTString();
-}
+    Cookie.prototype.getCookie = function (cName) {
+        var _value = "",
+            _cStart,
+            _cEnd,
+            doc = this.doc,
+            cookie = doc.cookie;
+        if (cName != null && cName !== "" && cookie.length > 0) {
+            _cStart = cookie.indexOf(cName + "=");
+            if (_cStart != -1) {
+                _cStart += (cName + "=").length;
+                _cEnd = cookie.indexOf(";", _cStart);
+                if (_cEnd == -1) {
+                    _cEnd = cookie.length;
+                }
+                _value = this.unescape(cookie.substring(_cStart, _cEnd));
+            }
+        }
+        return _value;
+    };
 
-w.Cookie = new Cookie();
+    Cookie.prototype.deleteCookie = function (cName) {
+        var exdate = new Date();
+        exdate.setTime(exdate.getTime() - 1);
+        var cval = this.getCookie(cName);
+        if (cval != null) this.doc.cookie = cName + "=" + cval + ";expires=" + exdate.toGMTString();
+    }
 
-}(window,undefined);
+    w.Cookie = new Cookie();
+
+}(window, void 0);
